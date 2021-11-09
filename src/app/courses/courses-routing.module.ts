079ecd9e91f826d3ef7c6f03405bd07d4df65ee5
chandni-suresh <chandnisuresh0224@gmail.com>
2021-11-09 14:36:25 -0800

    Routing, Lazy Loading and Loading commits

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,17 +1,36 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from '../page-not-found/page-not-found.component';
+import { CourseComponent } from './course/course.component';
+import { HomeComponent } from './home/home.component';
+import { CourseResolver } from './services/course.resolver';
 
 
 const routes: Routes = [
+  {
+
+    path:":courseUrl",
+    component: CourseComponent,
+    resolve:{
+      course:CourseResolver
+    }
+  },
+
+  {
+  path:"",
+  component: HomeComponent
+},
 
 ];
 
 @NgModule({
   imports: [
+    RouterModule.forChild(routes)
 
   ],
   exports: [RouterModule],
   providers: [
+    CourseResolver
 
   ]
 })
